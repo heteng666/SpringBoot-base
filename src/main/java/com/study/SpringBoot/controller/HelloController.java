83b89dbe713b8c2f23cb7a60aa35d253a752c33e
a278779191 <18001164010@163.com>
2019-05-24 17:04:58 +0800

    springboot提交

---
+++ src/main/java/com/study/SpringBoot/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.study.SpringBoot.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/study")
+	public String Study() {
+		return "Work Hard";
+		
+	}
+	
+}
---
+++ src/main/java/com/study/SpringBoot/controller/TestHelloController.java
@@ -0,0 +1,15 @@
+//package com.study.SpringBoot.controller;
+//
+//import javax.annotation.Resource;
+//
+//
+//@SpringBootTest(classes = HelloController.class)
+//public class TestHelloController {
+//
+//	@Resource
+//	private HelloController helloController;
+//	@Test
+//	public void testHome() {
+//		TestCase.assertEquals(this.sampleController.home(), "www.study.cn");
+//	}
+//}
