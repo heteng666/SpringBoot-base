28eccd57fd08009c521e11f4fecc5334947c7279
a278779191 <18001164010@163.com>
2019-06-03 15:38:40 +0800

    //测试

--- src/main/java/com/study/SpringBoot/controller/HelloController.java
+++ src/main/java/com/study/SpringBoot/controller/HelloController.java
@@ -1,6 +1,5 @@
 package com.study.SpringBoot.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
